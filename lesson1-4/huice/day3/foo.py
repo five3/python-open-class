0e901e25a3eb442a629970653734c61b3cd199ec
Xiaowu Chen <five3@163.com>
2018-12-01 17:38:40 +0800

    update

--- lesson1-4/huice/day3/foo.py
+++ lesson1-4/huice/day3/foo.py
@@ -1,3 +1,10 @@
-with open('bar.py', 'r', encoding='utf8') as f:
-    for line in f:
-        print(line)
+n = 0
+def foo():
+    m = 0
+    n = 1
+    print(locals())
+    print(globals())
+
+foo()
+print(locals())
+print(globals())
