681d8f238a8130d7c02c20bfe96fd76a60a943e2
chenxiaowu <chenxiaowu@autohome.com.cn>
2018-12-05 18:05:48 +0800

    fix bug

--- lesson1-4/huice/day1/homework.py
+++ lesson1-4/huice/day1/homework.py
@@ -1,15 +1,7 @@
-s = "asdasj678596knxcn213123982930knsdns,mnfsdh234898ncmdnsdkj"
-# 取出给定字符串中的数字，并打印出是偶数的字符，当打印次数大于10时退出
-# e.g. 上文会打印出：6862282024
+# 题1：取出给定字符串中的数字，并打印出是偶数的字符，当打印次数大于10时退出
+# e.g. 下面字符串会打印出：6862282024
+s1 = "asdasj678596knxcn213123982930knsdns,mnfsdh234898ncmdnsdkj"
 
-
-def find_max_count_ip(fn):
-    with open(fn, 'r', encoding='utf8') as f:
-        for ip in f:
-            print(ip, end='')
-            # TODO:
-
-
-if "__main__" == __name__:
-    find_max_count_ip('ip')
-    # 找到出现次数最多前N个IP
+# 题2：给定一个字符串，找出其中连续重复次数最多的子字符串。
+# e.g. 如下字符串会打印出：lllll
+s2 = "wqueiosfsdhfweeessqwueiqolllll"
--- lesson1-4/huice/day2/homework.py
+++ lesson1-4/huice/day2/homework.py
@@ -1,1 +1,1 @@
-"""课堂上未完成的""""""切割字符串统计每个单词出现次数    按次数排序""""""#扩展给定字符串，对单词进行反转e.g. hello python => olleh nohtyp"""s = 'hello python's2 = ' '.join([word[::-1] for word in s.split(' ')])print(s2)
+# 题1：给定一个IP文件（每行一个ip地址）。打印出重复次数最多的IP和对应的次数def find_max_count_ip(fn):    with open(fn, 'r', encoding='utf8') as f:        for ip in f:            print(ip, end='')            # TODO:if "__main__" == __name__:    find_max_count_ip('ip')    # 找到出现次数最多前N个IP"""# 题2：给定字符串，对单词进行反转e.g. hello python => olleh nohtyp"""
