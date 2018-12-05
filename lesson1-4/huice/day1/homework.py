a8e984a2c31ab1e2eed1edf207024f235c005616
chenxiaowu <chenxiaowu@autohome.com.cn>
2018-12-05 17:50:55 +0800

    fix bug

--- lesson1-4/huice/day1/homework.py
+++ lesson1-4/huice/day1/homework.py
@@ -1,5 +1,6 @@
 s = "asdasj678596knxcn213123982930knsdns,mnfsdh234898ncmdnsdkj"
 # 取出给定字符串中的数字，并打印出是偶数的字符，当打印次数大于10时退出
+# e.g. 上文会打印出：6862282024
 
 
 def find_max_count_ip(fn):
@@ -7,6 +8,7 @@ def find_max_count_ip(fn):
         for ip in f:
             print(ip, end='')
             # TODO:
+
 
 if "__main__" == __name__:
     find_max_count_ip('ip')
